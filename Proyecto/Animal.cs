c26db4d289e3f18ef33972c106c823cf7139393d
josecolula <jose.colula@bec.seguridad.unam.mx>
2020-02-16 11:49:02 -0600

    Apenas estoy aterrizando la idea xD

---
+++ Proyecto/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto
+{
+    class Animal : Producto
+    {
+        string raza;
+        double peso;
+        double altura;
+        string color;
+
+        public string Raza
+        {
+            get { return raza; }
+            set { raza = value; }
+        }
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+        
+    }
+}
---
+++ Proyecto/Perro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto
+{
+    class Perro
+    {
+        string pedigree;
+
+        public string Pedigree
+        {
+            get { return pedigree; }
+            set { pedigree = value; }
+        }
+    }
+}
