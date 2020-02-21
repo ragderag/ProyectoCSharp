5e229377182d218c87aae20a141ecf703271446f
ragderag <ragdeyo@gmail.com>
2020-02-21 06:29:39 -0600

    csv

--- proyecto/proyecto/Reptilescs.Designer.cs
+++ proyecto/proyecto/Reptilescs.Designer.cs
@@ -39,6 +39,10 @@
             this.button3 = new System.Windows.Forms.Button();
             this.Cantidad = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
@@ -47,10 +51,10 @@
             // 
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button1.Location = new System.Drawing.Point(1316, 38);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(987, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(189, 44);
+            this.button1.Size = new System.Drawing.Size(142, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "Menú Principal";
             this.button1.UseVisualStyleBackColor = true;
@@ -59,10 +63,10 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(348, 87);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Location = new System.Drawing.Point(261, 71);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(313, 24);
+            this.comboBox1.Size = new System.Drawing.Size(236, 21);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -71,9 +75,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(113, 87);
+            this.label1.Location = new System.Drawing.Point(85, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 39);
+            this.label1.Size = new System.Drawing.Size(132, 31);
             this.label1.TabIndex = 2;
             this.label1.Text = "Categoria";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -83,9 +88,10 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(113, 223);
+            this.label2.Location = new System.Drawing.Point(85, 181);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(161, 39);
+            this.label2.Size = new System.Drawing.Size(129, 31);
             this.label2.TabIndex = 3;
             this.label2.Text = "Opciones";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -93,20 +99,20 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(348, 223);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox2.Location = new System.Drawing.Point(261, 181);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(313, 24);
+            this.comboBox2.Size = new System.Drawing.Size(236, 21);
             this.comboBox2.TabIndex = 4;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox1.Location = new System.Drawing.Point(1151, 159);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(863, 129);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(355, 574);
+            this.pictureBox1.Size = new System.Drawing.Size(266, 466);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -114,10 +120,10 @@
             // button2
             // 
             this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button2.Location = new System.Drawing.Point(348, 514);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(261, 418);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(159, 65);
+            this.button2.Size = new System.Drawing.Size(119, 53);
             this.button2.TabIndex = 6;
             this.button2.Text = "Informacion";
             this.button2.UseVisualStyleBackColor = true;
@@ -125,10 +131,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(587, 514);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(440, 418);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(145, 65);
+            this.button3.Size = new System.Drawing.Size(109, 53);
             this.button3.TabIndex = 7;
             this.button3.Text = "Agregar";
             this.button3.UseVisualStyleBackColor = true;
@@ -139,27 +145,75 @@
             this.Cantidad.AutoSize = true;
             this.Cantidad.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.Cantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cantidad.Location = new System.Drawing.Point(120, 407);
+            this.Cantidad.Location = new System.Drawing.Point(90, 331);
+            this.Cantidad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Cantidad.Name = "Cantidad";
-            this.Cantidad.Size = new System.Drawing.Size(154, 39);
+            this.Cantidad.Size = new System.Drawing.Size(123, 31);
             this.Cantidad.TabIndex = 8;
             this.Cantidad.Text = "Cantidad";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(348, 423);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(261, 330);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.Size = new System.Drawing.Size(90, 30);
             this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(110, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 31);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Precio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(265, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 31);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "$0";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(411, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 31);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Total";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(539, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 31);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "$0";
             // 
             // Reptilescs
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1536, 838);
+            this.ClientSize = new System.Drawing.Size(1152, 681);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.Cantidad);
             this.Controls.Add(this.button3);
@@ -171,8 +225,8 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.MinimumSize = new System.Drawing.Size(1534, 820);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(1156, 674);
             this.Name = "Reptilescs";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reptiles";
@@ -196,5 +250,9 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label Cantidad;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- proyecto/proyecto/Reptilescs.cs
+++ proyecto/proyecto/Reptilescs.cs
@@ -22,8 +22,20 @@ namespace proyecto
         List<Producto> Cale = new List<Producto>();
         public Reptilescs()
         {
-            
             InitializeComponent();
+
+            label4.Visible = false;
+            label3.Visible = false;
+            IEnumerable<string> query =
+                (from array in lista
+                 select array[0]).Distinct();
+
+            foreach (var item in query)
+            {
+                comboBox1.Items.Add(item);
+            }
+
+            /*
             comboBox1.Items.Add("Reptiles");
             comboBox1.Items.Add("Comida");
             comboBox1.Items.Add("Complementos Alimenticios");
@@ -31,7 +43,8 @@ namespace proyecto
             comboBox1.Items.Add("Iluminacion");
             comboBox1.Items.Add("Decoracion");
             comboBox1.Items.Add("Calefaccion");
-
+            */
+            /*
             foreach (var i in lista)
             {
                 if (i[0] == "comida")
@@ -64,8 +77,9 @@ namespace proyecto
                     Producto aux = new Producto(i[1], Convert.ToDecimal(i[2]), i[3], i[5]);
                     Cale.Add(aux);
                 }
-
-            }
+            
+            }    
+            */
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -76,7 +90,11 @@ namespace proyecto
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            IEnumerable<string> query =
+                from array in lista
+                where array.Contains(comboBox1.SelectedItem.ToString()) && array.Contains(comboBox2.SelectedItem.ToString())
+                select array[5];
+            Producto aux = new Producto(comboBox2.ToString(), Convert.ToDecimal(label4.Text.Remove(0,1)), pictureBox1.ImageLocation, query.ToArray()[0]);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -91,6 +109,22 @@ namespace proyecto
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            label4.Visible = false;
+            label3.Visible = false;
+            IEnumerable<string> query =
+                (from array in lista
+                 where array.Contains(comboBox1.SelectedItem.ToString())
+                 select array[1]).Distinct();
+            comboBox2.Items.Clear();
+            comboBox2.ResetText();
+
+            foreach (var item in query)
+            {
+                comboBox2.Items.Add(item);
+            }
+            button2.Visible = false;
+            /*
+
             if (comboBox1.SelectedItem.ToString() == "Reptiles")
             {
                 comboBox2.Items.Clear();
@@ -152,12 +186,37 @@ namespace proyecto
                     comboBox2.Items.Add(i.nombre);
                 }
             }
-
+            */
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBox1.SelectedItem.ToString() == "Reptiles")
+            button2.Visible = true;
+            label4.Visible = true;
+            label6.Text = "$0.0";
+            numericUpDown1.Value = 1;
+            label3.Visible = true;
+            IEnumerable<string> query =
+                from array in lista
+                where array.Contains(comboBox1.SelectedItem.ToString()) && array.Contains(comboBox2.SelectedItem.ToString())
+                select array[2];
+
+            foreach (var item in query)
+            {
+                label4.Text = "$" + item;
+            }
+            label6.Text = "$" + Convert.ToString(numericUpDown1.Value * Convert.ToDecimal(label4.Text.Remove(0, 1)));
+
+            IEnumerable<string> query2 =
+                from array in lista
+                where array.Contains(comboBox1.SelectedItem.ToString()) && array.Contains(comboBox2.SelectedItem.ToString())
+                select array[3];
+            foreach (var item in query2)
+            {
+                pictureBox1.ImageLocation = item;
+            }
+
+            /*if (comboBox1.SelectedItem.ToString() == "Reptiles")
             {
 
             }
@@ -233,7 +292,7 @@ namespace proyecto
                     }
                 }
             }
-
+            */
 
         }
 
@@ -244,6 +303,26 @@ namespace proyecto
 
         private void button2_Click(object sender, EventArgs e)
         {
+            try
+            {
+                IEnumerable<string> query =
+                from array in lista
+                where array.Contains(comboBox1.SelectedItem.ToString()) && array.Contains(comboBox2.SelectedItem.ToString())
+                select array[5];
+                if (query.ToArray().Length > 0)
+                {
+                    foreach (var item in query)
+                    {
+                        MessageBox.Show(item);
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+
+            /*
             if (comboBox1.SelectedItem.ToString() == "Reptiles")
             {
 
@@ -308,11 +387,22 @@ namespace proyecto
                     }
                 }
             }
+            */
         }
 
         private void Reptilescs_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            label6.Text = "$" + Convert.ToString(numericUpDown1.Value * Convert.ToDecimal(label4.Text.Remove(0,1)));
         }
     }
 }
